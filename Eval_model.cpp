4536ec379cca470ac62781c74499add2363fdb4a
Sai Krishna <32714322+SaiKrishnaBV@users.noreply.github.com>
2020-03-30 08:00:01 +0530

    Update Eval_model.cpp

--- Eval_model.cpp
+++ Eval_model.cpp
@@ -2,12 +2,13 @@
 #include <torch/script.h>
 #include <iostream>
 
-const std::string pretrained_model_path = "/home/krishna/SK/Pytorch/traced_DQN_model.pth";
+const std::string pretrained_model_path = "/../.../..../traced_DQN_model.pth";
+//Replace "/../.../..../traced_DQN_model.pth" with the absolute path to the traced model of the trained neural network
 torch::jit::script::Module DQN;
 
 DQN = torch::jit::load(pretrained_model_path);
 
-auto sample_input = torch::randn({1, 4}); //Replace this with the observation from GAZEBO
+auto sample_input = torch::randn({1, 4}); //Replace this with the observation from environment
 std::vector<torch::jit::IValue> inputs{sample_input};
 
 auto output = DQN.forward(inputs).toTensor();
